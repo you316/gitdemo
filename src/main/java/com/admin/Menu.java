11f3aafee976a93dd0178c4ce83f8b54d3237b92
gitjohn <190791066@qq.com>
2019-02-19 09:02:02 +0800

    优化菜单类john111

--- src/main/java/com/admin/Menu.java
+++ src/main/java/com/admin/Menu.java
@@ -3,8 +3,8 @@ package com.admin;
 import java.util.Date;
 
 public class Menu {
-    private Long id;
-    private String menuName;
-    private String menuMemo;
+    private Integer id;
+    private String name;
+    private String memo;
     private Date createtime;
 }
