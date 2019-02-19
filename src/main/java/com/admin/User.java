24127d9d18b463f85903fe7c9e1fa31ca9f1a3ee
gitzhang <280333911@qq.com>
2019-02-19 16:19:09 +0800

    后台代码注释，小优化首页

--- src/main/java/com/admin/User.java
+++ src/main/java/com/admin/User.java
@@ -6,9 +6,13 @@ import java.util.Date;
 public class User {
     //id
     private Integer id;
+    //名称
     private String name;
+    //性别
     private String sex;
+    //年纪
     private String age;
+    //地址
     private String address;
     //状态
     private int status;
