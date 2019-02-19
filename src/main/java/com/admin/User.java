c44b5a3d653b05effb3896d94cb8127c0ae15b92
gitzhang <280333911@qq.com>
2019-02-19 11:20:18 +0800

    加注释

--- src/main/java/com/admin/User.java
+++ src/main/java/com/admin/User.java
@@ -4,10 +4,12 @@ import java.util.Date;
 import java.util.Date;
 
 public class User {
+    //id
     private Integer id;
     private String name;
     private String sex;
     private String age;
     private String address;
+    //时间
     private Date createtime;
 }
